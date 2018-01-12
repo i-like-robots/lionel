3a99e9212f32eee472f37dfe758b153719398665
Matt Hinchliffe <matt@maketea.co.uk>
2018-01-12 08:38:07 +0000

    Move message into list controller

--- controllers/list.js
+++ controllers/list.js
@@ -3,11 +3,15 @@ const fetchRSS = require('../lib/fetch-rss');
 module.exports = () => {
 	return fetchRSS('https://www.ft.com/?format=rss')
 		.then((items) => {
-			return items.map((item) => {
-				return {
+			const message = `Latest stories on FT.com ${new Date().toISOString()}`;
+
+			const choices = items.map((item) => (
+				{
 					name: item.title,
 					value: item.guid.text
-				};
-			})
-		})
+				}
+			));
+
+			return { choices, message };
+		});
 };
