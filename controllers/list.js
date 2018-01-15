ed59bacb48193099c70f0cb98534ca1472737ba6
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-01-15 15:48:34 +0000

    Add prompt to list

--- controllers/list.js
+++ controllers/list.js
@@ -3,11 +3,15 @@ const fetchRSS = require('../lib/fetch-rss');
 module.exports = () => {
 	return fetchRSS('https://www.ft.com/?format=rss')
 		.then((items) => {
-			return items.map((item) => (
+			const prompt = `Latest news on FT.com at ${new Date().toTimeString()}`;
+
+			const choices = items.map((item) => (
 				{
 					name: item.title,
 					guid: item.guid.text
 				}
 			));
+
+			return { prompt, choices };
 		});
 };
