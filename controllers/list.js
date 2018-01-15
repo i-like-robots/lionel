ea89aed3a8f73727acd41c223ea114064f06ecab
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-01-15 13:54:57 +0000

    Drop inquirer from homebrew UI

--- controllers/list.js
+++ controllers/list.js
@@ -3,15 +3,11 @@ const fetchRSS = require('../lib/fetch-rss');
 module.exports = () => {
 	return fetchRSS('https://www.ft.com/?format=rss')
 		.then((items) => {
-			const message = `Latest stories on FT.com ${new Date().toISOString()}`;
-
-			const choices = items.map((item) => (
+			return items.map((item) => (
 				{
 					name: item.title,
-					value: item.guid.text
+					guid: item.guid.text
 				}
 			));
-
-			return { choices, message };
 		});
 };
