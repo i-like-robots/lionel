44a249faa040b795ee4f56b676d0ba13d012e991
Matt Hinchliffe <matt@maketea.co.uk>
2019-03-17 17:32:15 +0000

    Fetch article list from content API to remove dependency on Elasticsearch

--- controllers/article-list.js
+++ controllers/article-list.js
@@ -1,16 +1,20 @@
-const fetchList = require('../lib/fetch-list');
+const api = require('../lib/content-api');
 const listUUIDs = require('../lib/list-uuids');
 
 const isNew = (date) => {
 	return Date.now() - Date.parse(date) < 1000 * 60 * 60;
 };
 
+const isArticle = (content) => {
+	return content.type === 'http://www.ft.com/ontology/content/Article';
+};
+
 module.exports = async (uuid) => {
 	const prompt = `The latest in ${listUUIDs.get(uuid)} at ${new Date().toTimeString()}`;
 
-	const items = await fetchList(uuid);
+	const items = await api.list(uuid);
 
-	const articles = items.filter((item) => item.type === 'article').slice(0, 10);
+	const articles = items.filter(isArticle).slice(0, 10);
 
 	const choices = articles.map((article) => {
 		return {
